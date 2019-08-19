8f8ace51264ac04252e50da82aef9e73d150c49d
Isa Milefchik <isavmilefchik@gmail.com>
2019-08-19 16:28:27 -0700

    Vectorization performance increase

--- convfade.py
+++ convfade.py
@@ -32,11 +32,21 @@ def main():
     if args.output == "":
         sys.exit("No output path given.")
 
-    start_w, start_sr = librosa.core.load(args.start, sr=None, mono=True)
-    end_w, end_sr = librosa.core.load(args.end, sr=None, mono=True)
+    start_w, start_sr = librosa.core.load(args.start, sr=None, mono=False)
+    end_w, end_sr = librosa.core.load(args.end, sr=None, mono=False)
 
-    start_dur = start_w.shape[0] / float(start_sr)
-    end_dur = end_w.shape[0] / float(end_sr)
+    # Check stereo vs. mono (makes both waveforms stereo if at least one is)
+    if len(start_w.shape) > 1 or len(end_w.shape) > 1:
+        if len(start_w.shape) == 1:
+            start_w = np.array([start_w, start_w])
+        if len(end_w.shape) == 1:
+            end_w = np.array([end_w, end_w])
+
+        start_dur = start_w.shape[1] / float(start_sr)
+        end_dur = end_w.shape[1] / float(end_sr)
+    else:
+        start_dur = start_w.shape[0] / float(start_sr)
+        end_dur = end_w.shape[0] / float(end_sr)
 
     if args.length > start_dur or args.length > end_dur:
         sys.exit("Length of fade greater than length of at least " \
@@ -49,6 +59,7 @@ def main():
     # Match sample rates:
     # --------------------------------------------------------------------------
 
+    print("Matching sample rates... ", end="")
     # Global sample rate conforms to highest sample rate
     sr = start_sr
     if start_sr > end_sr:
@@ -57,12 +68,23 @@ def main():
     if end_sr > start_sr:
         start_w = librosa.core.resample(start_w, start_sr, end_sr)
         sr = end_sr
+    print("done.")
 
     # --------------------------------------------------------------------------
     # Accomplish fade:
     # --------------------------------------------------------------------------
 
-    result = convfade(start_w, end_w, sr, args.length, args.frame)
+    print("Calculating ConvFade... ", end="") 
+    if len(start_w.shape) > 1:
+        print("\n   Stereo mode engaged.")
+        result_l = convfade(start_w[0], end_w[0], sr, args.length, args.frame)
+        print("   Left track done.")
+        result_r = convfade(start_w[1], end_w[1], sr, args.length, args.frame)
+        print("   Right track done.")
+        result = np.array([result_l, result_r])
+    else:
+        result = convfade(start_w, end_w, sr, args.length, args.frame)
+        print("done.")
 
     # --------------------------------------------------------------------------
     # Export result:
@@ -70,35 +92,35 @@ def main():
 
     librosa.output.write_wav(args.output, result, sr, norm=True)
 
-    sys.exit()
+    print("Exported to " + args.output)
 
 def convfade(start_w, end_w, sr, fade_len, frame_len):
     """ Accomplish a convolutional crossfade.
 
     Parameters:
-    start_w (np.array) -- The audio waveform that the fade begins with.
-    end_w (np.array) -- The audio waveform that the fade ends with.
+    start_w (np.array) -- Mono audio waveform that the fade begins with.
+    end_w (np.array) -- Mono audio waveform that the fade ends with.
     sr (int) -- Global sample rate (start_w and end_w should be matched
                 in sample rate beforehand).
     fade_len (float) -- Length of the fade in seconds.
     frame_len (int) -- Length of the STFT frames/windows.
 
     Returns:
-    np.array -- Waveform of start_w and end_w stitched together by the
+    np.array -- Mono waveform of start_w and end_w stitched together by the
                 convolutional crossfade.
     """
 
     # Fade length in number of samples
-    fade_len = int(sr * fade_len)
+    fade_len_s = int(sr * fade_len)
 
     # Sample index in start audio at which fade begins
-    fade_start = (start_w.shape[0] - 1) - fade_len
+    fade_start = (start_w.shape[0] - 1) - fade_len_s
 
     # Frame length in number of samples
-    frame_len = int((frame_len / 1000.) * sr)
+    frame_len_s = int((frame_len / 1000.) * sr)
 
-    start_stft = librosa.core.stft(start_w[fade_start:], n_fft=frame_len).T
-    end_stft = librosa.core.stft(end_w[0:fade_len], n_fft=frame_len).T
+    start_stft = librosa.core.stft(start_w[fade_start:], n_fft=frame_len_s).T
+    end_stft = librosa.core.stft(end_w[0:fade_len_s], n_fft=frame_len_s).T
     assert start_stft.shape == end_stft.shape, "STFT shapes not equal."
 
     # Calculate ConvFade
@@ -114,8 +136,6 @@ def convfade(start_w, end_w, sr, fade_len, frame_len):
 
         convolved = 8 * np.sqrt(np.multiply(start_scale * start_stft[i], \
                 end_scale * end_stft[i]))
-        convolved = convolved + ((1 - end_scale) * start_stft[i])
-        convolved = convolved + ((1 - start_scale) * end_stft[i])
 
         result_stft.append(convolved)
 
@@ -123,9 +143,27 @@ def convfade(start_w, end_w, sr, fade_len, frame_len):
     result_stft = np.array(result_stft)
     result_fade = librosa.core.istft(result_stft.T)
 
+
     # Stitching together final product
+    half_way = float(result_fade.shape[0] / 2)
+    
+    start_amp = np.repeat(int(half_way),int(half_way))
+    start_amp = np.append(np.arange(half_way), start_amp)
+    start_amp = np.sqrt(1 - (start_amp / half_way))
+    start_amp[int(half_way):] = 0
+
+    end_amp = np.arange(half_way, result_fade.shape[0])
+    end_amp = np.append(np.repeat(int(half_way),int(half_way)), end_amp)
+    end_amp = np.sqrt((end_amp - half_way) / half_way)
+    end_amp[:int(half_way)] = 0
+
+    result_fade = result_fade + np.multiply(start_amp, \
+            start_w[fade_start:fade_start + result_fade.shape[0]])
+    result_fade = result_fade + np.multiply(end_amp, \
+            end_w[0:result_fade.shape[0]])
+
     result = np.append(start_w[0:fade_start], result_fade)
-    result = np.append(result, end_w[fade_len - 1:])
+    result = np.append(result, end_w[fade_len_s:])
 
     return result
     
